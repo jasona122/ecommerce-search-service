653cfa832833353ad54e5579816d3148d6caef52
jasona122 <17jgunawan@gmail.com>
2023-03-24 01:12:21 +0700

    Fix shop service not initialized

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	s "github.com/jasona122/ecommerce-search-service/server"
 	"github.com/jasona122/ecommerce-search-service/service"
 	"github.com/jasona122/ecommerce-search-service/service/productsearch"
+	"github.com/jasona122/ecommerce-search-service/service/shopsearch"
 
 	"github.com/codegangsta/negroni"
 )
@@ -27,13 +28,16 @@ func main() {
 
 func initServices(configs config.Config) service.Services {
 	esConfigs := configs.GetElasticSearchConfig()
-	productESService, err := elasticsearch.NewProductSearchESService(esConfigs)
+	esService, err := elasticsearch.NewProductSearchESService(esConfigs)
 	if err != nil {
 		panic(fmt.Sprintf("could not initialize product ES service: %s", err))
 	}
-	productSearchService := productsearch.NewService(productESService)
+	productSearchService := productsearch.NewService(esService)
+
+	shopSearchService := shopsearch.NewService(esService)
 
 	return service.Services{
 		ProductSearchService: productSearchService,
+		ShopSearchService:    shopSearchService,
 	}
 }
